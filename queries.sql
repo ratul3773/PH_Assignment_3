5daf40780fbf22243348b5c071baf4ec41b14a9f
Robiul Alam <ratul1036c@gmail.com>
2025-12-23 15:07:49 +0600

    change

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+-- Retrieve booking information along with Customer name and Vehicle name.
 SELECT bookings.id AS booking_id,
        users.name AS customer_name,
        vehicles.vehicle_name,
@@ -8,6 +9,7 @@ FROM bookings JOIN users USING(user_id)
               JOIN vehicles USING(vehicle_id);
 
 
+-- Find all vehicles that have never been booked.
 SELECT vehicle_name AS name,
        type,
        model,
@@ -21,9 +23,11 @@ WHERE NOT EXISTS(
 )
     
 
+-- Retrieve all available vehicles of a specific type (e.g. cars).
 SELECT * FROM vehicles WHERE type = 'car' AND availability_status = 'available';
 
 
+-- Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
 SELECT vehicle_name,
        count(*) AS total_bookings
 FROM bookings JOIN vehicles USING (vehicle_id)
